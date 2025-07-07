4cede5e97b15452a219f07ca2e25f141756a0769
Erritix <erritix@users.noreply.github.com>
2025-07-07 14:30:52 +0700

    adding randomRange func

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const x = 100
+const x = 14
+const y = 4
 
-console.log(x)
+function random(from, to) {
+    return Math.floor((Math.random() * (to - from) + from) * 100) / 100
+}
+
+const randRange = random(y, x)
+
+console.log(randRange)
+
+for (let i = 1; i < 64; i++) {
+    console.log(random(32, 64))
+}
